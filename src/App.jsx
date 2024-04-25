d44ecff98fe61ba8e10f8503c5d930b208010b67
ebanchik <ebanchik@gmail.com>
2024-04-25 14:01:06 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "./Banner.jsx"
+import './App.css'
+
+function App() {
+
+  return (
+    <Banner/>
+  )
+}
+
+export default App
---
+++ src/Banner.jsx
@@ -0,0 +1,5 @@
+export function Banner() {
+  return (
+    <h1>PLEASE KILL ME</h1>
+  )
+}

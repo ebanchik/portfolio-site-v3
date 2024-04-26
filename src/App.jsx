defb5e4b9588056541f47e783b9f6809eef022b7
ebanchik <ebanchik@gmail.com>
2024-04-26 16:11:32 +0100

    new header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Banner } from "./Banner.jsx"
 import './App.css'
+import './Header.css'
 import Header from './Header';
 
 function App() {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+import './Header.css'; // Ensure this CSS file is in the same directory
+
 export const Header = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <header className="header">
-      <h1>My Header</h1>
+      <div 
+        className="hover-area-left" 
+        onMouseEnter={() => setIsHovering(true)} 
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        {isHovering && (
+          <div className="header-options-left">
+            <p>ABOUT</p>
+            <p>WORK</p>
+            <p>FF</p>
+            <p>LINKS</p>
+          </div>
+        )}
+        <div className="circle-left"></div>
+      </div>
     </header>
   );
 };

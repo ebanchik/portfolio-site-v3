01911adb3e24d7057d858199fe713c847463548a
ebanchik <ebanchik@gmail.com>
2024-04-26 16:35:12 +0100

    cascade down and up

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
 import './Header.css'; // Ensure this CSS file is in the same directory
 
 export const Header = () => {
-  const [isHovering, setIsHovering] = useState(false);
-
   return (
     <header className="header">
-      <div 
-        className="hover-area-left" 
-        onMouseEnter={() => setIsHovering(true)} 
-        onMouseLeave={() => setIsHovering(false)}
-      >
-        {isHovering && (
-          <div className="header-options-left">
-            <p style={{ animationDelay: '0s' }}>ABOUT</p>
-            <p style={{ animationDelay: '0.1s' }}>WORK</p>
-            <p style={{ animationDelay: '0.2s' }}>FF</p>
-            <p style={{ animationDelay: '0.3s' }}>LINKS</p>
-          </div>
-        )}
+      <div className="hover-area-left">
+        <div className="header-options-left">
+          <p style={{ '--delay-in': '0s', '--delay-out': '0.3s' }}>ABOUT</p>
+          <p style={{ '--delay-in': '0.1s', '--delay-out': '0.2s' }}>WORK</p>
+          <p style={{ '--delay-in': '0.2s', '--delay-out': '0.1s' }}>FF</p>
+          <p style={{ '--delay-in': '0.3s', '--delay-out': '0s' }}>LINKS</p>
+        </div>
         <div className="circle-left"></div>
       </div>
     </header>

16198790ca11ec1a33d8fd27c893d67544849237
ebanchik <ebanchik@gmail.com>
2024-04-29 15:45:02 +0100

    animations and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { Banner } from "./Banner.jsx"
 import './App.css'
 import './Header.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Banner } from "./Banner.jsx";
 import Header from './Header';
 import Projects from './Projects';
 import Skills from './Skills';
 import About from './About';
 
 function App() {
-
   return (
-    <div>
-      <div id="banner"><Banner /></div>
-      <div id="header"><Header /></div>
-      <div id="about"><About /></div>
-      <div id="projects"><Projects /></div>
-      <div id="skills"><Skills /></div>
-    </div>
-  )
+    <Router>
+      <div>
+        <div id="banner"><Banner /></div>
+        <div id="header"><Header /></div>
+        <Routes>
+          <Route path="/" element={<About />} /> {/* Default route */}
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          {/* Add more routes for other pages if needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;

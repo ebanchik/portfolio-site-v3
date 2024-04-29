5185be9fa2d6bcf3f812e12d63a0865ca9f5745f
ebanchik <ebanchik@gmail.com>
2024-04-29 13:52:18 +0100

    about text

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,9 @@
 export function About() {
   return (
-    <div className="about">
-      <h1>ABOUT</h1>
+    <div className="about-container">
+      <div className="about-text">
+        <h3>Ambitious Web Designer and Developer focusing on crafting engaging, dynamic, and performative web projects. Additional professional in  Marketing and Graphic Design compiles a well-rounded skillset to tackle any project simple or complex. </h3>
+      </div>
     </div>
   )
 }

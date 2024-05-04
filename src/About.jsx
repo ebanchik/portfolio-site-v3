d02ac5e40920afb0f1246ddd3f8d87843f2fecfb
ebanchik <ebanchik@gmail.com>
2024-05-04 18:12:31 +0100

    picture with frame

--- src/About.jsx
+++ src/About.jsx
@@ -3,13 +3,13 @@ import './About.css';
 export function About() {
   return (
     <div className="about-container">
+      <img className='linkedin-photo' src="../public/linkedin2.png" alt="LinkedIn Profile"/>
       <div className="about-text">
         <h3>
           Ambitious Web Designer and Developer focusing on crafting 
-          engaging, dyanmic, and performative web projects. Additional professional in Marketing and Graphic Design compiles a well-rounded skillset to tackle any project, simple or complex.
+          engaging, dynamic, and performative web projects. Additional professional in Marketing and Graphic Design compiles a well-rounded skillset to tackle any project, simple or complex.
         </h3>
       </div>
-      <img className='linkedin-photo' src="../public/linkedin.png"/>
     </div>
   );
 }

53ad31a1efe73ac4a6ab679947878f14525227a4
ebanchik <ebanchik@gmail.com>
2024-05-05 19:23:29 +0100

    parallax

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,21 @@
 
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 import './About.css';
 
 export function About() {
+  // Use Framer Motion hooks to control the motion
+  const { scrollYProgress } = useViewportScroll();
+  const y = useTransform(scrollYProgress, [0, 1], [-50, 25]);  // These values control the range of motion, adjust as needed
+
   return (
     <div className="about-container">
       <div className="circle-hover">
-        <img className='linkedin-photo' src="../public/linkedin2.png" alt="LinkedIn Profile"/>
+        <motion.img
+          className='linkedin-photo'
+          src="../public/linkedin2.png"
+          alt="LinkedIn Profile"
+          style={{ y }}  // Apply the motion effect to the y-axis
+        />
       </div>
       <div className="about-text-container">
         <h3 className="about-text">

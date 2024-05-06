48d8b689597d31ef9b6d490d09cd4c97e757a3dc
ebanchik <ebanchik@gmail.com>
2024-05-06 16:09:05 +0100

    add transform in chrome

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,37 +1,44 @@
 import { useState, useEffect } from 'react';
-
+import { motion } from 'framer-motion';
 
 export function Banner() {
-  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+    const [rotation, setRotation] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      const { clientX, clientY } = event;
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-      const xVal = clientX - width / 2;
-      const yVal = clientY - height / 2;
-      const yRotation = 20 * (xVal / width); // 20 degrees max rotation on mouse move
-      const xRotation = -20 * (yVal / height); // 20 degrees max rotation on mouse move
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            const { clientX, clientY } = event;
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            const xVal = clientX - width / 2;
+            const yVal = clientY - height / 2;
+            const yRotation = 20 * (xVal / width); // Max rotation 20 degrees based on mouse move
+            const xRotation = -20 * (yVal / height); // Max rotation -20 degrees based on mouse move
 
-      setRotation({ x: xRotation, y: yRotation });
-    };
+            setRotation({ x: xRotation, y: yRotation });
+        };
 
-    window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mousemove', handleMouseMove);
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
 
-  return (
-    <div className="banner">
-      <img className="chrome"
-           src="../public/eli-chrome.png"
-           style={{ transform: `rotateY(${rotation.y}deg) rotateX(${rotation.x}deg)` }}
-           alt="Descriptive Alt Text"/>
-    </div>
-  )
+    return (
+        <div className="banner">
+            <motion.img className="chrome"
+                src="../public/eli-chrome.png"
+                alt="Descriptive Alt Text"
+                initial={{ scale: 1.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1, transition: { duration: 1.5 } }}
+                style={{
+                    rotateX: rotation.x,
+                    rotateY: rotation.y,
+                    transformOrigin: 'center'
+                }}
+            />
+        </div>
+    );
 }
 
 export default Banner;

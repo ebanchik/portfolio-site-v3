6c454196e7d7ec14c61c20b16bd8594e65d2a35b
ebanchik <ebanchik@gmail.com>
2024-05-06 17:46:45 +0100

    keyboard

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,8 +4,11 @@ export function Projects() {
   return (
     <div className="project-container">
       <h1 className='projects-title'>PROJECTS</h1>
+      <div className="keyboard-container">
+        <img className='keyboard' src="../public/keyboard.svg" height="500px" alt="Keyboard"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;

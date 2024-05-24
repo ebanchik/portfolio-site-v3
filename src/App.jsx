bc9a7841a01b7a9dc68c605bc06b854c0cd44513
ebanchik <ebanchik@gmail.com>
2024-05-24 16:37:32 +0100

    fix nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import './App.css';
 import './Header.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Banner } from "./Banner.jsx";
 import Header from './Header';
 import Projects from './Projects';
 import Skills from './Skills';
 import About from './About';
 import ScrollProgressBar from './ScrollProgressBar';
+
 function App() {
   return (
     <div>
-    <ScrollProgressBar />
-    <Router>
-      <div>
-        <div id="header"><Header /></div>
-        <div id="banner"><Banner /></div>
-        <div id="about"><About /></div>
-        {/* <div id="skills"><Skills /></div> */}
-        <div id="projects"><Projects /></div>
-        {/* Add more sections as needed */}
-        <div id="skills"><Skills /></div>
-      </div>
-    </Router>
+      <ScrollProgressBar />
+      <Router>
+        <div>
+          <div id="header"><Header /></div>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/" element={<Banner />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,23 @@
 import './Header.css'; // Ensure this CSS file is in the same directory
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="hover-area-left">
         <div className="header-options-left">
-          <p style={{ '--delay-in': '0s', '--delay-out': '0.3s' }}>ABOUT</p>
-          <p style={{ '--delay-in': '0.1s', '--delay-out': '0.2s' }}>WORK</p>
-          <p style={{ '--delay-in': '0.2s', '--delay-out': '0.1s' }}>FF</p>
-          <p style={{ '--delay-in': '0.3s', '--delay-out': '0s' }}>LINKS</p>
+          <Link to="/about" style={{ '--delay-in': '0s', '--delay-out': '0.3s' }}>
+            <p>ABOUT</p>
+          </Link>
+          <Link to="/projects" style={{ '--delay-in': '0.1s', '--delay-out': '0.2s' }}>
+            <p>WORK</p>
+          </Link>
+          <Link to="/ff" style={{ '--delay-in': '0.2s', '--delay-out': '0.1s' }}>
+            <p>FF</p>
+          </Link>
+          <Link to="/links" style={{ '--delay-in': '0.3s', '--delay-out': '0s' }}>
+            <p>LINKS</p>
+          </Link>
         </div>
         <img src="/plus_sign.png" alt="Profile" className="circle-left"/>
       </div>

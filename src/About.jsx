da74f14f599e407d329a0b197b9b60f3108956b0
ebanchik <ebanchik@gmail.com>
2024-05-27 14:34:54 +0100

    fix about text

--- src/About.jsx
+++ src/About.jsx
@@ -20,7 +20,7 @@ export function About() {
       <div className="about-text-container">
         <h3 className="about-text">
           Ambitious Web Designer and Developer focusing on crafting 
-          engaging, dynamic, and performative web projects. Additional professional in Marketing and Graphic Design compiles a well-rounded skillset to tackle any project, simple or complex.
+          engaging, dynamic, and performative web projects. Additional professional skills in Marketing and Graphic Design composes a well-rounded skillset to tackle any project, simple or complex.
         </h3>
       </div>
     </div>
